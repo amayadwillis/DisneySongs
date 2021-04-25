9e27230d761514201290f0277c7063cd7c2f8ad3
wdjacca <chanjacqueline0418@gmail.com>
2021-04-25 17:03:30 -0400

    more testing

---
+++ docs/images/network1.tsv_3/web_session/styles.js
@@ -0,0 +1,55 @@
+var styles = [ {
+  "format_version" : "1.0",
+  "generated_by" : "cytoscape-3.8.2",
+  "target_cytoscapejs_version" : "~2.1",
+  "title" : "default",
+  "style" : [ {
+    "selector" : "node",
+    "css" : {
+      "color" : "rgb(0,0,0)",
+      "width" : 75.0,
+      "text-valign" : "center",
+      "text-halign" : "center",
+      "shape" : "roundrectangle",
+      "border-color" : "rgb(204,204,204)",
+      "background-opacity" : 1.0,
+      "border-width" : 0.0,
+      "font-size" : 12,
+      "height" : 35.0,
+      "font-family" : "SansSerif.plain",
+      "font-weight" : "normal",
+      "text-opacity" : 1.0,
+      "background-color" : "rgb(137,208,245)",
+      "border-opacity" : 1.0,
+      "content" : "data(name)"
+    }
+  }, {
+    "selector" : "node:selected",
+    "css" : {
+      "background-color" : "rgb(255,255,0)"
+    }
+  }, {
+    "selector" : "edge",
+    "css" : {
+      "font-family" : "Dialog.plain",
+      "font-weight" : "normal",
+      "source-arrow-color" : "rgb(0,0,0)",
+      "content" : "",
+      "color" : "rgb(0,0,0)",
+      "width" : 2.0,
+      "target-arrow-shape" : "none",
+      "opacity" : 1.0,
+      "font-size" : 10,
+      "line-style" : "solid",
+      "target-arrow-color" : "rgb(0,0,0)",
+      "source-arrow-shape" : "none",
+      "text-opacity" : 1.0,
+      "line-color" : "rgb(132,132,132)"
+    }
+  }, {
+    "selector" : "edge:selected",
+    "css" : {
+      "line-color" : "rgb(255,0,0)"
+    }
+  } ]
+} ]
