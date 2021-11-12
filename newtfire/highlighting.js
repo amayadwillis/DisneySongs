9ea39d15240f5995468c9b1b828a5f30772f6e21
wdjacca <chanjacqueline0418@gmail.com>
2021-11-11 22:59:06 -0500

    committing old JS changes

--- newtfire/highlighting.js
+++ newtfire/highlighting.js
@@ -1,21 +1,5 @@
 // JavaScript Document
 
-function searchBar() {
-/* alert('hey! this function fired!');  */
-let input = document.getElementById("input");
-let filter = input.value.toUpperCase();
-let searchAreas = document.getElementsByTagName("p");
-for (var i = 0, length = searchAreas.length; i < length; i++) 
-{
-      if (searchAreas[i].innerHTML.toUpperCase().indexOf(filter) > -1) {
-        searchAreas[i].style.display = "";
-      } else {
-        searchAreas[i].style.display = "none";
-      }
-    }
-}
-
-
 function init()
 {
 
